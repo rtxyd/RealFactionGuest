3d7a8d0fafbc42910a979d1217f40666d47f2d41
rtxyd <65117312+rtxyd@users.noreply.github.com>
2025-07-29 02:21:26 +0800

    fix mod setting

--- Source/EventController/RealFactionGuestSettings.cs
+++ Source/EventController/RealFactionGuestSettings.cs
@@ -7,20 +7,21 @@ namespace EventController_rQP
         /// <summary>
         /// The three settings our mod has.
         /// </summary>
-        public static bool strictQuestGuest;
-        public static bool alternativeFaction;
-        public static float strictChance;
-        public static bool factionLeaderValidator;
-        public static bool debugOption;
-        public static bool damageUntilDownedBypassShield;
-        public static bool dontAdjustXenotypeForRabbie;
-        public static bool creepJoinerValidator;
-        public static bool creepJoinerGenerateNoLimit;
+        public static bool strictQuestGuest = true;
+        public static bool alternativeFaction = false;
+        public static float strictChance = 1.0f;
+        public static bool factionLeaderValidator = true;
+        public static bool debugOption = false;
+        public static bool damageUntilDownedBypassShield = true;
+        public static bool dontAdjustXenotypeForRabbie = true;
+        public static bool creepJoinerValidator = true;
+        public static bool creepJoinerGenerateNoLimit = true;
         /// <summary>
         /// The part that writes our settings to file. Note that saving is by ref.
         /// </summary>
         public override void ExposeData()
         {
+            base.ExposeData();
             Scribe_Values.Look(ref strictQuestGuest, "strictQuestGuest", true);
             Scribe_Values.Look(ref factionLeaderValidator, "factionLeaderValidator", true);
             Scribe_Values.Look(ref damageUntilDownedBypassShield, "damageUntilDownedBypassShield", true);
@@ -40,7 +41,6 @@ namespace EventController_rQP
             }
             Scribe_Values.Look(ref alternativeFaction, "alternativeFaction", false);
             Scribe_Values.Look(ref strictChance, "guestChance", 1.0f);
-            base.ExposeData();
         }
         public static void ResetToDefault()
         {
